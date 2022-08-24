7a21a7db3446ca6347c89775968ce91cbdab4e18
April <aprilphuong@gmail.com>
2022-08-24 15:57:49 -0700

    Update filename for #include in main.c

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
  * 				   handlers implemented - SIGNINT and SIGTSTP.
  ****************************************************************************/
 
-#include "smallsh.c"
+#include "simpleCshell.c"
 
 int main(int argc, char *argv[]) {
 	// Instantiate our command struct
